fa9cec309fdeaa28d4105a653d10fbf4148047d0
Roland Kallai <kallai.roland@outlook.com>
2019-06-16 23:02:11 +0200

    gitignore fix

--- app/controllers/aboutus.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class AboutUs extends Controller {
-
-
-    public function index() {
-        $this->view("aboutusview");
-    }
-}
--- public/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once('..\\app\\autoload.php');
-require_once('..\\app\\autoload_views.php');
-
-$app=new App();
